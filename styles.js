2b6bd6201ef93058e85e907b131368d9e5a7cf52
isaqkee <isaquecorreia34@gmail.com>
2023-08-21 16:31:23 -0300

    Finalizado.

--- styles.js
+++ styles.js
@@ -1,96 +1,90 @@
-const question = [
+const options = document.querySelectorAll('.option');
+const scoreDisplay = document.getElementById('score');
+const questionImage = document.getElementById('question-image');
+const questionText = document.getElementById('question-text');
+
+let score = 0;
+let chances = 2;
+let currentQuestion = 0;
+
+options.forEach(option => {
+    option.addEventListener('click', checkAnswer);
+});
+
+const questions = [
     {
-      options: ['a) Vaticano e Rússia', 'b) Nauru e China', 'c) Mônaco e Canadá', 'd) Malta e Estados Unidos', 'e) São Marino e Índia'],
-      correctAnswer: 0
-      
+        question: 'Quais o menor e o maior país do mundo?',
+        image: 'img/mapa-mundi.jpg',
+        options: ['Vaticano e Rússia', ' Nauru e China', ' Nauru e China', 'São Marino e Índia'],
+        answer: 'Vaticano e Rússia'
     },
     {
-      correctAnswer: 3,
-      options: ['a) 113', 'b) 109', 'c) 108', 'd) 118', 'e) 92']
+        image: 'img/tabela-periodica.jpg',
+        question: 'Atualmente, quantos elementos químicos a tabela periódica possui?',
+        options: ['113', '109', '108', '118'],
+        answer: '118'
     },
     {
-      correctAnswer: 4,
-      options: ['a) 12 minutos', 'b) 1 hora', 'c) 12 horas', 'd) 8 minutos', 'e) 12 segundos']
+        image: 'img/sol.jpg',
+        question: 'Quanto tempo a luz do Sol demora para chegar à Terra?',
+        options: ['8 minutos', '12 minutos', '12 segundos', '4 minutos'],
+        answer: '8 minutos'
     },
     {
-      correctAnswer: 0,
-      options: ['a) Terra, Vênus, Saturno, Urano, Júpiter, Marte, Netuno, Mercúrio', 'b) Júpiter, Marte, Mercúrio, Netuno, Plutão, Saturno, Terra, Urano, Vênus', 'c) Júpiter, Marte, Mercúrio, Netuno, Plutão, Saturno, Terra, Urano, Vênus', 'd) Júpiter, Marte, Mercúrio, Netuno, Plutão, Saturno, Sol, Terra, Urano, Vênus', 'e) Terra, Vênus, Saturno, Júpiter, Marte, Netuno, Mercúrio']
+        image: 'img/astronomia.jpg',
+        question: 'Júpiter e Plutão são os correlatos romanos de quais deuses gregos?',
+        options: ['Ares e Hermes', 'Cronos e Apolo', 'Zeus e Hades', 'Zeus e Ares'],
+        answer: 'Zeus e Hades'
+    },
+    {
+        image: 'img/animais.jpg',
+        question: 'Qual o maior animal terrestre?',
+        options: ['Lula Terrestre', 'Hipopótamo', 'Elefante africano', 'Baleia Azul'],
+        answer: 'Elefante africano'
     }
-  ];
-  
-  const optionsButtons = document.querySelectorAll('.opcoes');
-  const nextButton = document.querySelector('.next-button');
-  
-  let currentQuestionIndex = 0;
-  let answered = false;
-  
-  optionsButtons.forEach(button => {
-    button.addEventListener('click', () => handleAnswer(button));
-  });
-  
-  nextButton.addEventListener('click', () => showNextQuestion());
-  
-  function handleAnswer(selectedOption) {
-    if (answered) return;
-    answered = true;
-  
-    optionsButtons.forEach(button => {
-      button.classList.add('incorrect');
-      if (button.textContent === questions[currentQuestionIndex].correctAnswer) {
-        button.classList.remove('incorrect');
-        button.classList.add('correct');
-      }
-    });
-  
-    selectedOption.classList.add('selected');
-  
-    nextButton.disabled = false;
-  }
-  
-  function showNextQuestion() {
-    answered = false;
-    nextButton.disabled = true;
-    optionsButtons.forEach(button => {
-      button.classList.remove('incorrect', 'correct', 'selected');
-    });
-  
-    currentQuestionIndex++;
-  
-    if (currentQuestionIndex < questions.length) {
-      loadQuestion(currentQuestionIndex);
-    } else {
-      showResult();
-    }
-  }
-  
-  function loadQuestion(index) {
-    const currentQuestion = questions[index];
-    const options = currentQuestion.options;
-  
-    optionsButtons.forEach((button, i) => {
-      button.textContent = options[i];
-    });
-  
-    nextButton.disabled = true;
-  }
-  
-  function showResult() {
-    const container = document.querySelector('.container');
-    container.innerHTML = `
-      <h1>Resultado</h1>
-      <p>Seu resultado foi: ${calculateScore()} pontos!</p>
-    `;
-  }
-  
-  function calculateScore() {
-    let score = 0;
-    optionsButtons.forEach(button => {
-      if (button.classList.contains('correct')) {
+];
+
+function checkAnswer(event) {
+    const selectedOption = event.target;
+    
+    if (selectedOption.textContent === questions[currentQuestion].answer) {
         score += 10;
-      }
+        scoreDisplay.textContent = score;
+        currentQuestion++;
+        
+        if (currentQuestion < questions.length) {
+            loadQuestion(currentQuestion);
+        } else {
+            alert('Parabéns! Já pode estudar no Sesi!!!');
+        }
+    } else {
+        chances--;
+        if (chances === 0) {
+            alert('Volta pra escola agora.');
+            resetQuiz();
+        } else {
+            alert('Vou ser humilde vai, Mais uma CHANCE!.');
+        }
+    }
+}
+
+function loadQuestion(questionIndex) {
+    const currentQuestionData = questions[questionIndex];
+    
+    questionImage.src = currentQuestionData.image;
+    questionText.textContent = currentQuestionData.question;
+    
+    options.forEach((option, index) => {
+        option.textContent = currentQuestionData.options[index];
     });
-    return score;
-  }
-  
-  loadQuestion(currentQuestionIndex);
-  
+}
+
+function resetQuiz() {
+    score = 0;
+    chances = 2;
+    currentQuestion = 0;
+    scoreDisplay.textContent = score;
+    loadQuestion(currentQuestion);
+}
+
+loadQuestion(currentQuestion);
